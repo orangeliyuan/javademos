2ffaf4ca1ed5975c58df8954ddd4888316330681
Liyuan21742 <yuan.li08@hand-china.com>
2019-08-03 22:46:20 +0800

    day3 push1

--- java8/src/main/java/per/example/java8/stream/demo22.java
+++ java8/src/main/java/per/example/java8/stream/demo22.java
@@ -3,6 +3,6 @@ package per.example.java8.stream;
 public class demo22 {
 
     public static void main(String[] args) {
-
+        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "12");
     }
 }
